fab60c314feaf271dcd0059edcfe17d2be5ab74f
danile71 <danile71@gmail.com>
2018-11-08 22:30:56 +0300

    Fix

--- a55ml_dtul/Android.mk
+++
@@ -1,7 +0,0 @@
-ifeq ($(TARGET_DEVICE),a55ml_dtul)
-LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-include $(call all-makefiles-under,$(LOCAL_PATH))
-include $(CLEAR_VARS)
-
-endif
--- a55ml_dtul/AndroidProducts.mk
+++
@@ -1,3 +0,0 @@
-LOCAL_PATH := device/htc/a55ml_dtul
-PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_a55ml_dtul.mk
